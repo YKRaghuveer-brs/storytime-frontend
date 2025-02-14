6279e4bd4d01c41e4c390c21ada1bb38ac4e9130
unknown <yelurikesavaraghuveer@gmail.com>
2025-02-14 14:58:34 +0530

    storytime-frontend-ijypj.onrender.com

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
   server: {
     port:  process.env.PORT || 3000, // Use process.env.PORT if it's available
     host: '0.0.0.0', // Ensure Vite binds to all network interfaces
+    allowedHosts: ['storytime-frontend-ijypj.onrender.com'], // add your host here
   },
 })
